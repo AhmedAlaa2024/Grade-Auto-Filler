fa56c53616e4f8bc821c752ab5a42ab7c4a092c5
BeshoyMorad <besh0morta@gmail.com>
2022-12-14 23:34:26 +0200

    Integrate cell extractor with main module and first successful test

--- Code/Final Code/GradesSheet.py
+++ Code/Final Code/GradesSheet.py
@@ -1,3 +1,4 @@
+from CellsExtractionPhase import main
 from DetectionPhase import detectionPhase
 from ExtractExcel import generate_excel
 import cv2
@@ -5,15 +6,16 @@ import cv2
 # =============================================================================================
 # Extract cells from the table
 # =============================================================================================
-img1 = cv2.imread("../Samples/Detection phase samples/right1.png")
-img2 = cv2.imread("../Samples/Detection phase samples/question mark3.png")
-img3 = cv2.imread("../Samples/Detection phase samples/digits/9.png")
+# img1 = cv2.imread("../Samples/Detection phase samples/right1.png")
+# img2 = cv2.imread("../Samples/Detection phase samples/question mark3.png")
+# img3 = cv2.imread("../Samples/Detection phase samples/digits/9.png")
 
-img4 = cv2.imread("../Samples/Detection phase samples/english name2.png")
-img5 = cv2.imread("../Samples/Detection phase samples/arabic name3.png")
-img6 = cv2.imread("../Samples/Detection phase samples/code1.png")
+# img4 = cv2.imread("../Samples/Detection phase samples/english name2.png")
+# img5 = cv2.imread("../Samples/Detection phase samples/arabic name3.png")
+# img6 = cv2.imread("../Samples/Detection phase samples/code1.png")
 
-cellImages = [img1, img2, img3, img4, img5, img6]
+# cellImages = [img1, img2, img3, img4, img5, img6]
+cellImages = main('../Samples/Samples/15.jpg')
 
 # =============================================================================================
 # Extract the data from the cell images
