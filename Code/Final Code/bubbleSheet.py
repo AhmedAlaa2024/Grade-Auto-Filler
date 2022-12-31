c53440443d875ab4916600521a917ba59bf4f4ed
BeshoyMorad <besh0morta@gmail.com>
2022-12-31 22:29:51 +0200

    Integrate bubble sheet auto corrector with excel extractor function

--- Code/Final Code/bubbleSheet.py
+++ Code/Final Code/bubbleSheet.py
@@ -1,5 +1,6 @@
 from extractExcel import generateBubbleSheetExcel
 from readConfigFiles import readConfigBubbleSheet
+from bubbleSheetAutoCorrector import bubbleSheetAutoCorrector
 
 # =============================================================================================
 # Read the configuration file
@@ -9,7 +10,7 @@ config = readConfigBubbleSheet()
 # =============================================================================================
 # Get the data from the bubble sheet image
 # =============================================================================================
-data = yourFunction()
+data = bubbleSheetAutoCorrector(config)
 
 # =============================================================================================
 # Prepare the excel sheet
