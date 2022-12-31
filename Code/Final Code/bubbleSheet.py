3e0b65e2801e3d8fb79ead53c888deadff07a29a
BeshoyMorad <besh0morta@gmail.com>
2022-12-31 22:39:47 +0200

    Remove useless imports

--- Code/Final Code/bubbleSheet.py
+++ Code/Final Code/bubbleSheet.py
@@ -1,11 +1,3 @@
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import bar
-from imutils import contours
-import numpy as np
-import argparse
-import imutils
-import cv2
-
 from extractExcel import generateBubbleSheetExcel
 from readConfigFiles import readConfigBubbleSheet
 from bubbleSheetAutoCorrector import bubbleSheetAutoCorrector
