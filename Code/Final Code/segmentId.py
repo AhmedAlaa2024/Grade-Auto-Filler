824b7309683f30e4af4e108e2dab33881d3ec8c8
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-31 18:34:41 +0200

    update segment id

--- Code/Final Code/segmentId.py
+++ Code/Final Code/segmentId.py
@@ -3,45 +3,55 @@ import numpy as np
 from getPredection import getPrediction
 
 
-def segement(img):
-		img = 255*img
-		kernel = np.ones((5, 5), np.uint8)
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-		edged = cv2.Canny(blurred, 50, 200, 255)
-		
-		# contours, hierarchy = cv2.findContours((edged).astype("uint8"),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-		contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-		contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr))
-		white_img_large_contours = np.ones(img.shape)
-		dimensions_contours = []
-		for contour in contours:
-				(x, y, w, h) = cv2.boundingRect(contour)
-				if(w*h > 50):
-						dimensions_contours.append((x, y, w, h))
-						cv2.rectangle(white_img_large_contours,(x, y), (x+w, y+h), (0, 0, 255), 3)
-		return dimensions_contours, img
+def segmentId(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = 255 * img
+    img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    res, img = cv2.threshold(img, 64, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours((img).astype("uint8"), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr))
+    white_img_large_contours = np.ones(img.shape)
+    dimensions_contours = []
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if (w * h > 40):
+            dimensions_contours.append((x, y, w, h))
+            cv2.rectangle(white_img_large_contours, (x, y), (x + w, y + h), (0, 0, 255), 1)
+    cropped_digits = []
+    i = 0
+    filtered_img = img
+    for dimension in dimensions_contours:
+        (x, y, w, h) = dimension
+        imgCopy = filtered_img[y - 1:y + h + 1, x - 1:x + w + 1]
+        x = imgCopy.shape[1]
+        number_of_images = 1
+        if (x > 25):
+            number_of_images = np.ceil(x / 23.0)
+            for j in range(int(number_of_images)):
+                imgTemp = cv2.resize(imgCopy[:, j * 23:(j + 1) * 23], (200, 100))
+                imgTemp = cv2.resize(imgTemp, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+                cropped_digits.append(imgTemp)
+                i += 1
+        else:
+            if imgCopy.size == 0:
+                continue
+            imgCopy = cv2.resize(imgCopy, (200, 100))
+            cropped_digits.append(imgCopy)
+            imgCopy = cv2.resize(imgCopy, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+            i += 1
+
+    return cropped_digits
 
 
 # A function used to extract the id from image
 # it takes the image then segment the digits from it and get the prediction from each image
 def getIdFromImage(img):
-	segmented_dimensions, filtered_img = segement(img)
-	cropped_digits = []
-	i = 0
-	for dimension in segmented_dimensions:
-		(x, y, w, h) = dimension
-		cropped_digits.append(filtered_img[y-1:y+h+1, x-1:x+w+1])
-		i += 1
+    print("Entered")
+    cropped_digits = segmentId(img)
+    predictedNumber = ""
 
-	predictions = []
-
-	for img in cropped_digits:
-		predictions.append(getPrediction(img))
-
-	predictedNumber = ""
-	for number in predictions:
-		predictedNumber += str(number)
-
-	return predictedNumber
-
+    for img in cropped_digits:
+        # print(getPrediction(img))
+        predictedNumber += str(getPrediction(img))
+    return predictedNumber
